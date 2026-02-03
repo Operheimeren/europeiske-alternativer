505183a3ccc971f43669dcd5138ca92a8cd496b9
Operheimeren <thomas.opheim@protonmail.com>
2026-02-03 18:24:56 +0100

    Fix prerender configuration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
 <script>
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
